a332f9a5db80f6b04719c6efca6d3903248a68b4
Anthony <khaiatanthony1@gmail.com>
2024-08-13 21:44:58 -0400

    changed date format

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import joblib
 import pandas as pd
+from datetime import datetime
 
 # If your logo is stored locally in your repo, use a relative path
 LOGO_URL_LARGE = "https://static.wixstatic.com/media/71ac90_328ecaed562c43a0ba2fb4ba8fe777aa~mv2.jpg/v1/crop/x_818,y_1584,w_2531,h_1013/fill/w_460,h_180,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/name-change-2lvc-rgb.jpg" 
@@ -33,15 +34,23 @@ st.header('By Anthony Khaiat')
 
 # User input for the features
 years_in_business = st.number_input('Years in Business', min_value=0)
-days_since_first_financing = st.number_input('Days Since First Financing', min_value=0)
-total_raised = st.number_input('Total Raised', min_value=0.0, format="%.2f")
-last_financing_size = st.number_input('Last Financing Size', min_value=0.0, format="%.2f")
-competitor_count = st.number_input('Competitor Count', min_value=0)
-ownership_status_acquired_merged = st.selectbox('Ownership Status: Acquired/Merged (Operating Subsidiary)', [0, 1])
-days_since_last_financing = st.number_input('Days Since Last Financing', min_value=0)
-most_recent_employment_year = st.number_input('Most Recent Employment Year', min_value=0)
-number_of_employees = st.number_input('# Employees', min_value=0)
-third_most_recent_employment_year = st.number_input('Third Most Recent Employment Year', min_value=0)
+
+# Use st.date_input to take a date input from the user
+first_financing_date = st.date_input('First Financing Date')
+today_date = datetime.today().date()
+days_since_first_financing = (today_date - first_financing_date).days
+
+# Use st.date_input for the last financing date and calculate days since last financing
+last_financing_date = st.date_input('Last Financing Date')
+days_since_last_financing = (today_date - last_financing_date).days
+
+total_raised = st.number_input('Total Raised (in millions USD)', min_value=0.0, format="%.2f")
+last_financing_size = st.number_input('Last Financing Size (in millions USD)', min_value=0.0, format="%.2f")
+competitor_count = st.number_input('Number of Competitors', min_value=0)
+ownership_status_acquired_merged = st.selectbox('Was the startup previously acquired or merged? (0 = No, 1 = Yes)', [0, 1])
+most_recent_employment_year = st.number_input('How many employees did the startup have 1 year ago?', min_value=0)
+number_of_employees = st.number_input('What is the current number of employees?', min_value=0)
+third_most_recent_employment_year = st.number_input('How many employees did the startup have 3 years ago?', min_value=0)
 
 # Combine user input into a single DataFrame
 input_data = {
